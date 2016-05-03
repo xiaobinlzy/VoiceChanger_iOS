cafc6045a9ed96637684c2629bf8b579c4f334d6
xiaobinlzy <xiaobinlzy@163.com>
2016-05-04 00:16:30 +0800

    修正了之前的错误，适配新版环境

---
+++ VoiceChanger/VoiceChanger/Libs/Speex/Classes/RecorderManager.h
@@ -0,0 +1,55 @@
+//
+//  RecorderManager.h
+//  OggSpeex
+//
+//  Created by Jiang Chuncheng on 6/25/13.
+//  Copyright (c) 2013 Sense Force. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+//#import "AQRecorder.h"
+#import "Encapsulator.h"
+
+@class VoiceChanger;
+
+@protocol RecordingDelegate <NSObject>
+
+- (void)recordingFinishedWithFileName:(NSString *)filePath time:(NSTimeInterval)interval;
+- (void)recordingTimeout;
+- (void)recordingStopped;  //录音机停止采集声音
+- (void)recordingFailed:(NSString *)failureInfoString;
+@optional
+- (void)levelMeterChanged:(float)levelMeter;
+
+@end
+
+@interface RecorderManager : NSObject <EncapsulatingDelegate> {
+    
+    Encapsulator *encapsulator;
+    NSString *filename;
+    NSDate *dateStartRecording;
+    NSDate *dateStopRecording;
+    NSTimer *timerLevelMeter;
+    NSTimer *timerTimeout;
+    BOOL _isRecording;
+}
+
+@property (nonatomic, weak)  id<RecordingDelegate> delegate;
+@property (nonatomic, strong) Encapsulator *encapsulator;
+@property (nonatomic, strong) NSDate *dateStartRecording, *dateStopRecording;
+@property (nonatomic, strong) NSTimer *timerLevelMeter;
+@property (nonatomic, strong) NSTimer *timerTimeout;
+
++ (RecorderManager *)sharedManager;
++ (NSString *)defaultFileName;
+
+- (void)startRecordingWithFilePath:(NSString *)filePath andVoiceChanger:(VoiceChanger *)voiceChanger;
+
+- (void)stopRecording;
+
+- (void)cancelRecording;
+
+- (NSTimeInterval)recordedTimeInterval;
+
+
+@end
---
+++ VoiceChangerDemo/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  VoiceChangerDemo
+//
+//  Created by DLL on 14-5-6.
+//  Copyright (c) 2014年 DLL. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "VoiceChangerAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([VoiceChangerAppDelegate class]));
+    }
+}
